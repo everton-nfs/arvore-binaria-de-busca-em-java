d658c39a66dbb217c70cde3e5805a3fdb27eaa1e
Everton Novaes <evertonnfs2@gmail.com>
2024-09-11 12:09:19 -0300

    feat: criando o arquivo do nó da arvore

---
+++ src/main/java/com/everton/binarysearchtree/BIntNo.java
@@ -0,0 +1,12 @@
+package main.java.com.everton.binarysearchtree;
+
+public class BIntNo {
+  int valor;
+  BIntNo esq, dir;
+
+  public BIntNo(int valor) {
+      this.valor = valor;
+      this.esq = null;
+      this.dir = null;
+  }
+}
